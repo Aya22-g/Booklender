1f614f0f6d2e3de10ce7cada53ffa46a38fbf18d
Aya22-g <moon22aikou@gmail.com>
2025-07-01 12:33:18 +0600

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,23 +1,16 @@
-public class Employee {String id, String name, String login, String password) {
-    this.id = id;
-    this.name = name;
-    this.login = login;
-    this.password = password;
-    this.booksIssued = new ArrayList<>();
-    this.history = new ArrayList<>();
-}
+public class Employee {
+    private long id;
+    private String fullName;
+    private String email;
+    private String password;
 
-    // Getters and Setters
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getLogin() { return login; }
-    public void setLogin(String login) { this.login = login; }
+    public Employee(long id, String fullName, String email, String password) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+    }
+    public long getId() { return id; }
+    public String getFullName() { return fullName; }
+    public String getEmail() { return email; }
     public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-    public List<String> getBooksIssued() { return booksIssued; }
-    public void setBooksIssued(List<String> booksIssued) { this.booksIssued = booksIssued; }
-    public List<String> getHistory() { return history; }
-    public void setHistory(List<String> history) { this.history = history; }
-}
